321b4cc9f42e87c62e4e14f6bff9437cc8f84256
lAkagiWhiteCometl <turovierov@mail.ru>
2022-04-11 13:50:19 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Comma_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Two_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Three_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Four_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Six_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Seven_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Eight_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Nine_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Erase_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sign_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Percent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Division_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
